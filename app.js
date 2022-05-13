17eda7cf51d29ff68e55d6099d0e8975ab566fdc
Spencer Krull <spencerkrull138@gmail.com>
2022-05-12 19:58:04 -0600

    changed category 'random' to 'internet;' wrote some trivia questions

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 
 // add buttons for multiple choice questions in each box, random for each, maybe 20-30 questions per main categories (not random)
 
-//  for correct answers, add marker to space; incorrect, allow player to steal; return to regular space if both wrong
+// for correct answers, add marker to space; incorrect, allow player to steal; return to regular space if both wrong
 
-// bingo = xxx player wins; no bingo = highest score wins
+// xxx player wins
 
 // scorecard for whoever has most squares
