58b83163bde99e9119be31b1c9a89e853b53a1ed
Spencer Krull <spencerkrull138@gmail.com>
2022-05-21 16:21:14 -0600

    tried to assign correct answers, have coded self into corner

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-// add event listeners for clicking each box
-// let player1 = window.prompt('Player 1, enter your name:')
-// let player2 = window.prompt('Player 2, enter your name:')
-let score1 = 0
-let score2 = 0
+let playerScore = 0
 let playAgain = true
 document.getElementById("modal").style.visibility = "hidden"; // default visibility = false
-
-// add buttons for multiple choice questions in each box, random for each, maybe 20-30 questions per main categories (not random)
 
 function displayMusicQuestion() {
     let element = document.querySelectorAll("#music")
--- questionbank.js
+++ questionbank.js
@@ -6,7 +6,7 @@ let musicQuestions = [
         B: '“Material Girl” - Madonna',
         C: '“Purple Rain” - Prince',
         D: '"Never Gonna Give You Up” - Rick Astley',
-        correctAnswer: "D"
+        correctAnswer: "D",
     },
     {
         text: `What song went from being considered “one of the worst ever written” to “one of the most covered songs in music history?”`,
